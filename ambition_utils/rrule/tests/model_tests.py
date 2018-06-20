aece5575215ff26c6d69574110f68127d3f2ec00
Wes Okes <wes.okes@ambition.com>
2018-06-20 12:59:46 -0400

    restore task test code

--- ambition_utils/rrule/tests/model_tests.py
+++ ambition_utils/rrule/tests/model_tests.py
@@ -18,6 +18,122 @@ class MockHandler(OccurrenceHandler):
         return True
 
 
+class HandlerOne(OccurrenceHandler):
+
+    def handle(self):
+        return RRule.objects.filter(
+            occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerOne',
+            next_occurrence__lte=datetime.datetime.utcnow(),
+        ).order_by('id')
+
+
+class HandlerTwo(OccurrenceHandler):
+
+    def handle(self):
+        return RRule.objects.filter(
+            occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerTwo',
+            next_occurrence__lte=datetime.datetime.utcnow(),
+        ).order_by('id')
+
+
+class HandlerThree(OccurrenceHandler):
+
+    def handle(self):
+        return RRule.objects.filter(
+            occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerThree',
+            next_occurrence__lte=datetime.datetime.utcnow(),
+        ).order_by('id')
+
+
+class RRuleManagerTest(TestCase):
+
+    def test_handle_overdue(self):
+        """
+        This is the method that should be called in a recurring task
+        """
+
+        @freeze_time('1-1-2017')
+        def test_run(self):
+            """
+            Should return the classes that have overdue rrule objects
+            """
+
+            # Make a program with HandlerOne that is not overdue
+            params = {
+                'freq': rrule.DAILY,
+                'interval': 1,
+                'dtstart': datetime.datetime(2017, 1, 2),
+            }
+
+            G(
+                RRule,
+                rrule_params=params,
+                occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerOne'
+            )
+
+            # Make an overdue program with HandlerOne
+            params = {
+                'freq': rrule.DAILY,
+                'interval': 1,
+                'dtstart': datetime.datetime(2017, 1, 1),
+            }
+
+            G(
+                RRule,
+                rrule_params=params,
+                occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerOne'
+            )
+
+            # Make an overdue program with HandlerTwo
+            params = {
+                'freq': rrule.DAILY,
+                'interval': 1,
+                'dtstart': datetime.datetime(2017, 1, 1),
+            }
+
+            G(
+                RRule,
+                rrule_params=params,
+                occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerTwo'
+            )
+
+            # Make a program with HandlerThree that is not overdue
+            params = {
+                'freq': rrule.DAILY,
+                'interval': 1,
+                'dtstart': datetime.datetime(2017, 1, 2),
+            }
+
+            G(
+                RRule,
+                rrule_params=params,
+                occurrence_handler_path='ambition_utils.rrule.tests.task_tests.HandlerThree'
+            )
+
+            # Get and verify the classes
+            classes = {
+                instance.__class__
+                for instance in RRule.objects.overdue_handler_class_instances()
+            }
+
+            self.assertEqual(classes, {HandlerOne, HandlerTwo})
+
+            # Handle overdue rrules
+            RRule.objects.handle_overdue()
+
+            # Check the recurrences. ids 2 and 3 should be updated
+            recurrences = list(RRule.objects.order_by('id'))
+
+            self.assertEqual(recurrences[0].next_occurrence, datetime.datetime(2017, 1, 2))
+            self.assertEqual(recurrences[1].next_occurrence, datetime.datetime(2017, 1, 2))
+            self.assertEqual(recurrences[2].next_occurrence, datetime.datetime(2017, 1, 2))
+            self.assertEqual(recurrences[3].next_occurrence, datetime.datetime(2017, 1, 2))
+
+            # For coverage, make handler 3 overdue
+            with freeze_time('1-3-2017'):
+                RRule.objects.handle_overdue()
+
+
 class RRuleTest(TestCase):
 
     def test_get_next_occurrence_first_is_occurrence(self):
