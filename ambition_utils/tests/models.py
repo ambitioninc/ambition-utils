7ef4ce573849e6d868d1741011c9c928de497f99
Wes Okes <somewes@gmail.com>
2023-01-24 12:07:59 -0500

    add time zone field and tests

--- ambition_utils/tests/models.py
+++ ambition_utils/tests/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from timezone_field.fields import TimeZoneField as BaseTimeZoneField
+
+from ambition_utils.fields import TimeZoneField
 
 
 class FakeModel(models.Model):
     name = models.CharField(max_length=50)
+    cast_time_zone_field = TimeZoneField(default='utc')
+    no_cast_time_zone_field = BaseTimeZoneField(default='utc')
