edcbaca2722009ef5c8856584dc791c6c7242296
Wes Okes <somewes@gmail.com>
2023-01-24 12:55:16 -0500

    migration to allow null

--- ambition_utils/tests/models.py
+++ ambition_utils/tests/models.py
@@ -6,5 +6,5 @@ from ambition_utils.fields import TimeZoneField
 
 class FakeModel(models.Model):
     name = models.CharField(max_length=50)
-    cast_time_zone_field = TimeZoneField(default='utc')
-    no_cast_time_zone_field = BaseTimeZoneField(default='utc')
+    cast_time_zone_field = TimeZoneField(default='utc', null=True)
+    no_cast_time_zone_field = BaseTimeZoneField(default='utc', null=True)
